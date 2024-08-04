300edfba9e37d87b29535f661d6e9eb92a0787e0
Pratik Maurya <155305739+Pratikmaury@users.noreply.github.com>
2024-08-04 07:16:50 +0530

    my commit

---
+++ calculator/calculator.js
@@ -0,0 +1,11 @@
+let first_num = document.querySelector("#one");
+let sec_num = document.querySelector("#two");
+let third_num = document.querySelector("#three");
+let forth_num = document.querySelector("#four");
+let fifth_num = document.querySelector("#five");
+let six_num = document.querySelector("#six");
+let seven_num = document.querySelector("#seven");
+let eight_num = document.querySelector("#eight");
+let nine_num = document.querySelector("#nine");
+let zero_num = document.querySelector("#zero");
+
